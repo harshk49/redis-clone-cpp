4482682c6e3dccebe2bd1c2e252f40ea70148c50
Harsh Kardile <harshkardile49@gmail.com>
2026-04-10 17:24:11 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -50,11 +50,22 @@ int main(int argc, char **argv) {
   // You can use print statements as follows for debugging, they'll be visible when running tests.
   std::cout << "Logs from your program will appear here!\n";
 
-  // Uncomment the code below to pass the first stage
-  // 
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  if (client_fd < 0) {
+    std::cerr << "Failed to accept client connection\n";
+    close(server_fd);
+    return 1;
+  }
   std::cout << "Client connected\n";
-  
+
+  // Respond with +PONG\r\n to any command (hardcoded for this stage)
+  const char *pong_response = "+PONG\r\n";
+  ssize_t bytes_sent = send(client_fd, pong_response, strlen(pong_response), 0);
+  if (bytes_sent < 0) {
+    std::cerr << "Failed to send response to client\n";
+  }
+
+  close(client_fd);
   close(server_fd);
 
   return 0;
